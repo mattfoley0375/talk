a051b5fc74c54bf54732d3762fff9298d8636f28
Matt Foley <mattfoley0375@gmail.com>
2021-01-12 15:35:57 -0800

    Add README
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
     $("ul#user").prepend("<li>Goodbye!</li>");
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
   });
-
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
